f6a25a709a8641f5ced4f2ab62c25cc2517db56e
reichi <sreichholf@users.noreply.github.com>
2021-03-19 23:10:43 +0100

    setup: bump version to 1.0.3

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     packages=find_packages(include=['dreamboxapi']),
     setup_requires=setup_requirements,
     url='https://github.com/sreichholf/python-dreamboxapi',
-    version='1.0.2',
+    version='1.0.3',
     zip_safe=False,
 )
